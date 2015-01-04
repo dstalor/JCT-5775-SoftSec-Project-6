4e233352399dfeb0b97f8c60700838c6e4dc6196
grogilius <grogilius@gmail.com>
2015-01-04 23:07:52 +0200

    start of hex_view get-lines (text output, not HTML)

--- psd/psd.py
+++ psd/psd.py
@@ -10,11 +10,21 @@ class psd:
         self.psd_project = psd_Project(self, filename)
 
 
+def test_HexView(psd_project):
+    from psd_visualizer.View import HexView
+    hv = HexView(psd_project.pe,(0,1000))
+    hv.get_lines((0,15))
 
 if __name__=="__main__":
     psd_main = psd()
     psd_main.create_new_project("examples\\helloworld.exe")
 
     psd_main.psd_project.pe.print_info()
+
+    #tests
+    test_HexView(psd_main.psd_project)
+
+
+
 
 
--- psd/psd_visualizer/View.py
+++ psd/psd_visualizer/View.py
@@ -22,14 +22,23 @@ class HexView(View):
         self._offset = offset
 
     def calculate_line_count(self):
-        return self.find_line_by_address(self.address_range[1])
+        return self.find_line_by_address(self._address_range[1])
 
     def get_lines(self, line_range_tup):
         (line_start, line_end) = line_range_tup
         address_start = self.find_address_by_line(line_start)
-        for l in range(line_start, line_end+1):
-            
+        fmt="".join(['{'+str(i)+':02x} ' for i in range(self._offset)])
+        str_out=""
+        img = self._pe.get_memory_mapped_image()
+        #"{0:02x}".format(ord(img[0]))
 
+        for l in range(line_start, line_end+1):
+            bytes = map(ord, img[address_start:address_start+self._offset])
+            str_out+=fmt.format(*bytes)+"\n"
+            address_start+=self._offset
+
+        print str_out
+        return str_out
 
     def find_line_by_address(self, address):
         bytes = (address-self._address_range[0])+1
@@ -37,7 +46,6 @@ class HexView(View):
 
     def find_address_by_line(self, line):
         return (line * self._offset) + self._address_range[0]
-
 
 
 if __name__ == "__main__":
