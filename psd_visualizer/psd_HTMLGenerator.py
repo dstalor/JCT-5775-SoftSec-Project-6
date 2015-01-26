fae04bc93d2ff155949b34d83b54ddffc27f831b
grogilius <grogilius@gmail.com>
2015-01-26 23:26:36 +0200

    lineschunk

--- psd_visualizer/psd_HTMLGenerator.py
+++ psd_visualizer/psd_HTMLGenerator.py
@@ -5,3 +5,6 @@ class psd_HTMLGenerator(object):
     def html_line_wrap(self, line_num, line_str):
         parity = "line-even" if line_num % 2 == 0 else "line-odd"
         return "<span class=\"line {0}\" id=\"{1:d}\">{2}</span>".format(parity, line_num, line_str)
+
+    def html_lineschunk_wrap(self, lineschunk_id, lineschunk_str):
+        return "<span class=\"linechunk\" id=\"{1:d}\">{2}</span>".format(lineschunk_id, lineschunk_str)
