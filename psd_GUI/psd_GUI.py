2d204a4a1ae1ca9659765c51e27b39400bec2c3e
grogilius <grogilius@gmail.com>
2015-01-26 18:35:15 +0200

    GUI class cleanup

--- psd_GUI/psd_GUI.py
+++ psd_GUI/psd_GUI.py
@@ -1,9 +1,37 @@
 import sys
-
+import resources
 from PyQt4 import QtGui, QtWebKit
+from PyQt4.QtCore import *
 
-from psd_EditorGUI import *
+class JsHooks(QObject):
+    def __init__(self, psd, psd_gui):
+        QObject.__init__(self)
+        self.psd_project = psd.psd_project
+        self.visualizer = self.psd_project.visualizer
+        self.psd_gui  = psd_gui
+        resources.qInitResources()
 
+    def get_all_html_lines(self):
+        return self.visualizer.get_all_html_lines()
+
+    @pyqtSlot(str, result=int)
+    def get_line_id_by_address(self, address):
+        return self.visualizer.get_line_id_by_address(int(str(address), 16))
+
+    @pyqtSlot(str, str)
+    def patch_byte_by_rva(self, rva_address, byte_val_str):
+        rva_address_int = int(str(rva_address), 16)
+        self.psd_project.patch_byte_by_rva(rva_address_int, str(byte_val_str))
+
+    @pyqtSlot()
+    def run_executable(self):
+        self.psd_project.run_executable()
+
+    @pyqtSlot()
+    def load_executable(self):
+        self.psd_gui.load_executable_gui()
+
+    visualizer_lines = pyqtProperty(str, fget=get_all_html_lines)
 
 class psd_GUI(object):
     def __init__(self, psd):
@@ -12,6 +40,7 @@ class psd_GUI(object):
         self.main_window = None
         resources.qInitResources()
         self.app = QtGui.QApplication(sys.argv)
+        self.bridge  = JsHooks(self.psd, self)
 
     def get_filename_dialog(self):
         dialog = QtGui.QFileDialog()
@@ -26,25 +55,22 @@ class psd_GUI(object):
     def create_main_window(self):
         self.main_window = QtGui.QMainWindow()
 
-    def create_editor_view(self, psd_project):
+    def create_main_view(self):
         webview = QtWebKit.QWebView()
         webview.setContextMenuPolicy(Qt.NoContextMenu)
-        self.editor_view = psd_EditorGUI(psd_project, self, webview)
-        self.main_window.setCentralWidget(webview)
+        self.main_view = webview
+        self.main_window.setCentralWidget(self.main_view)
+        with open("psd_GUI/base.html", "r") as myfile:
+            self.html = myfile.read()
+
+        self.main_view.page().mainFrame().addToJavaScriptWindowObject("pyObj", self.bridge)
+        self.main_view.setHtml(self.html)
 
     def run(self):
         self.create_main_window()
-
-        self.create_editor_view(self.psd.psd_project)
-
-        self.editor_view.update_view()
+        self.create_main_view()
 
         self.main_window.showMaximized()
         self.main_window.activateWindow()
 
         sys.exit(self.app.exec_())
-
-# if __name__ == '__main__':
-# psd_main = psd()
-# psd_main.create_new_project("examples\\helloworld.exe")
-# psd_GUI = psdGUI(psd_main)
